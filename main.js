0c4255495ca7b4b22ff91f2217cac2c0b298ee1e
ludndev <ludndev@gmail.com>
2023-09-19 21:58:45 +0100

    feat: use of sendPayloadToWebhook

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 require('dotenv').config()
 const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
+const util = require('./util');
 
 const client = new Client();
 
 client.on('qr', (qr) => {
     console.log('QR RECEIVED', qr);
     qrcode.generate(qr, {small: true});
+    util.sendPayloadToWebhook({
+        on: 'qr',
+        msg: 'qr_received'
+    });
 });
 
 client.on('ready', () => {
     console.log('Client is ready!');
+    util.sendPayloadToWebhook({
+        on: 'ready',
+        msg: 'client_ready'
+    });
 });
 
 client.initialize();
