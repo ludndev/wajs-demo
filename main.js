72288ff37765d48c23f6b8c3bb3325e25753eb04
ludndev <ludndev@gmail.com>
2023-09-19 21:34:26 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const qrcode = require('qrcode-terminal');
+const { Client } = require('whatsapp-web.js');
+
+const client = new Client();
+
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
